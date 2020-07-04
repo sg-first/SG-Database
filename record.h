1a99078b2b15d215313ab0e2f35ee3261f100dfd
3A <524019351@qq.com>
2020-07-05 02:36:41 +0800

    改变量名

--- record.h
+++ record.h
@@ -13,7 +13,7 @@ private:
     void setAddTarget(vector<Basic*> addTarget) //“增”时需要指定增进去的元组，会自动拷贝
     {
         for(Basic* v : addTarget)
-            this->addTarget.push_back(helper::copy(v));
+            this->targetTuple.push_back(helper::copy(v));
     }
 
 public:
@@ -34,15 +34,15 @@ public:
 
     record(const record &r) : opSub(r.opSub), type(r.type)
     {
-        this->setAddTarget(r.addTarget);
+        this->setAddTarget(r.targetTuple);
     }
 
     ~record()
     {
-        for(Basic* v : addTarget)
+        for(Basic* v : targetTuple)
             delete v;
     }
 
-    vector<Basic*> addTarget; //仅当type!=del时使用，变更之后的元组，placeholder对象为不修改
+    vector<Basic*> targetTuple; //仅当type!=del时使用，变更之后的元组，placeholder对象为不修改
 };
 
