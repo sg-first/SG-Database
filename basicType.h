55832aaf26c40f3b5165337e8e60af388f539a2f
3A <524019351@qq.com>
2020-07-05 21:48:14 +0800

    Basic.toStr

--- basicType.h
+++ basicType.h
@@ -9,12 +9,14 @@ class Basic
 public:
     virtual string getType() { return "Null"; } //直接创建的basic对象视为null
     virtual ~Basic() {}
+    virtual string toStr() { return "Null"; }
 };
 
 class Placeholder : public Basic
 {
 public:
     virtual string getType() override { return "Placeholder"; }
+    virtual string toStr() override { return "Placeholder"; }
 };
 
 class Int : public Basic
@@ -23,6 +25,7 @@ public:
     int val;
     virtual string getType() override {return "Int";}
     Int(int val) : val(val) {}
+    virtual string toStr() override { return to_string(val); }
 };
 
 class Float : public Basic
@@ -31,6 +34,7 @@ public:
     float val;
     virtual string getType() override {return "Float";}
     Float(float val) : val(val) {}
+    virtual string toStr() override { return to_string(val); }
 };
 
 class Str : public Basic
@@ -39,6 +43,7 @@ public:
     string val;
     virtual string getType() override {return "Str";}
     Str(string val) : val(val) {}
+    virtual string toStr() override { return "\""+val+"\""; }
 };
 
 class Bool : public Basic
@@ -47,4 +52,11 @@ public:
     bool val;
     virtual string getType() override {return "Bool";}
     Bool(bool val) : val(val) {}
+    virtual string toStr() override
+    {
+        if(val)
+            return "true";
+        else
+            return "false";
+    }
 };
